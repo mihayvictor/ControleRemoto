a159319c12235df79255aa9df9cb4242a5a045f5
Mihay Victor <m.victorcezario@gmail.com>
2024-05-23 15:46:44 -0300

    .

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,12 +7,25 @@ public class Main {
         Controle controleTV = new Controle();
 
         controleTV.definirCanal(13);
+        controleTV.verCanal();
+        controleTV.verVolume();
+        controleTV.maisVolume();
+        controleTV.menosVolume();
+        controleTV.definirVolume(99);
         controleTV.power();
+        controleTV.definirVolume(100);
         controleTV.verCanal();
         controleTV.definirCanal(2);
+        controleTV.definirCanal(100);
         controleTV.maisCanais();
         controleTV.maisCanais();
         controleTV.menosCanais();
+        controleTV.verVolume();
+        controleTV.maisVolume();
+        controleTV.menosVolume();
+        controleTV.mute();
+        controleTV.maisVolume();
+        controleTV.menosVolume();
         controleTV.power();
     
 
--- src/modelo/Controle.java
+++ src/modelo/Controle.java
@@ -3,7 +3,6 @@ package modelo;
 public class Controle {
 
     private int volume, canal;
-   
     private boolean power;
 
     public Controle(){
@@ -12,47 +11,75 @@ public class Controle {
         power = false;
     }
 
-    public void power() {
-        if (isPower() == false){
-            setPower(true);
+    public boolean power() {
+        setPower();
+        if (power == true) {
             System.out.println("TV ligada");
         }else{
-            setPower(false);
             System.out.println("TV desligada");
         }
-        
+        return isPower();
     }
 
     public void maisCanais() {
-        if (getCanal() >= 0) {
-            setCanal(canal+=1);   
-        } verCanal();
+        if (getCanal() == 100) {
+            setCanal(canal=0);
+        }else{
+            setCanal(canal+=1);
+        } 
+        verCanal();
     }
 
-    public int menosCanais() {
-        if (getCanal() >= 0) {
+    public void menosCanais() {
+        if (getCanal() == 0) {
+            setCanal(canal=100);
+        }else{
             setCanal(canal-=1);
-        }return verCanal();
-        
-    }
+        }
+            verCanal();
+        }
 
-    public int definirCanal(int mudarCanal) {
-            setCanal(mudarCanal);
-        return verCanal();
+    public int definirCanal(int canal) {
+            setCanal(canal);
+            return verCanal();
     }
 
     public int verCanal(){
-        System.out.println("Canal " + getCanal());
-        return getCanal();   
+         if (power == true) {
+            System.out.println("Canal " + getCanal());
+         }return getCanal();
     }
 
     public void verVolume() {
-        System.out.println("Volume " + getVolume());
+        getVolume();
     }
 
-    public void definirVolume(int Mudarvolume) {
-        setVolume(Mudarvolume);
-        System.out.println("Volume " + getVolume());
+    public void definirVolume(int volume) {
+        setVolume(volume);
+        verVolume();
+    }
+
+    public void maisVolume(){
+        if (volume < 100) {
+            setVolume(volume+=1);
+            verVolume();
+        }
+    }
+
+    public void menosVolume(){
+        if (volume > 0) {
+            setVolume(volume-=1);
+            verVolume();
+        }   
+    }
+
+    public void mute(){
+        if (volume != 0) {
+            setVolume(volume=0);
+        }else{
+            setVolume(getVolume()+1);
+        }    
+        verVolume();
     }
 
     private int getCanal() {
@@ -62,12 +89,13 @@ public class Controle {
 
     private void setCanal(int canal) {
         if (isPower() == true){
-        this.canal = canal;
+            this.canal = canal;
         }
     }
 
     private int getVolume() {
         if (isPower() == true){
+            System.out.println("Volume " + volume);
         }return volume;
     }
 
@@ -78,12 +106,10 @@ public class Controle {
     }
 
     private boolean isPower() {
-        return power;
-        
+        return this.power;
     }
 
-    private void setPower(boolean power) {
-        this.power = power;
-    }
-    
+    private void setPower() {
+        power = !isPower();
+        }
 }
