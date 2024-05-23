7409706b710c45f2f43516fdb76b6f368b915d16
Mihay Victor <m.victorcezario@gmail.com>
2024-05-23 16:04:56 -0300

    comentários

--- src/modelo/Controle.java
+++ src/modelo/Controle.java
@@ -5,12 +5,16 @@ public class Controle {
     private int volume, canal;
     private boolean power;
 
+    //Métodos marcados com * geralmente não são encontrados botões em controles
+
+    //Construtor de controle
     public Controle(){
         volume = 10;
         canal = 10;
         power = false;
     }
 
+    //Botão de power (ligar/desligar)
     public boolean power() {
         setPower();
         if (power == true) {
@@ -21,6 +25,7 @@ public class Controle {
         return isPower();
     }
 
+    //Botão de mudar canais para + 
     public void maisCanais() {
         if (getCanal() == 100) {
             setCanal(canal=0);
@@ -30,6 +35,7 @@ public class Controle {
         verCanal();
     }
 
+    //Botão de mudar canais para -
     public void menosCanais() {
         if (getCanal() == 0) {
             setCanal(canal=100);
@@ -37,28 +43,33 @@ public class Controle {
             setCanal(canal-=1);
         }
             verCanal();
-        }
-
+        }    
+    
+    //Realizar mudança de canal diretamente
     public int definirCanal(int canal) {
             setCanal(canal);
             return verCanal();
     }
 
+    //Botão para verificar qual o canal *
     public int verCanal(){
          if (power == true) {
             System.out.println("Canal " + getCanal());
          }return getCanal();
     }
 
+    //Botão para verificar qual o volume *
     public void verVolume() {
         getVolume();
     }
 
+    //Definir volume diretamente *
     public void definirVolume(int volume) {
         setVolume(volume);
         verVolume();
     }
 
+    //Botão para mudar volume para +
     public void maisVolume(){
         if (volume < 100) {
             setVolume(volume+=1);
@@ -66,6 +77,7 @@ public class Controle {
         }
     }
 
+    //Botão para mudar volume para -
     public void menosVolume(){
         if (volume > 0) {
             setVolume(volume-=1);
@@ -73,6 +85,7 @@ public class Controle {
         }   
     }
 
+    //Botão mute (colocar e tirar do mudo) 
     public void mute(){
         if (volume != 0) {
             setVolume(volume=0);
@@ -82,6 +95,7 @@ public class Controle {
         verVolume();
     }
 
+    //métodos de acesso (getter e setter) ao canal
     private int getCanal() {
         if (isPower() == true){
         }return canal;
@@ -92,19 +106,21 @@ public class Controle {
             this.canal = canal;
         }
     }
-
+    
+    //métodos de acesso (getter e setter) ao volume
     private int getVolume() {
         if (isPower() == true){
             System.out.println("Volume " + volume);
         }return volume;
     }
-
+ 
     private void setVolume(int volume) {
         if (isPower() == true){
         this.volume = volume;
         }
     }
 
+    //métodos de acesso (is e setter) ao power
     private boolean isPower() {
         return this.power;
     }
