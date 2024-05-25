4a31833913576eaebb76f37b7e0f927e9ac59530
Mihay Victor <m.victorcezario@gmail.com>
2024-05-25 17:58:13 -0300

    update

--- src/main/Main.java
+++ src/main/Main.java
@@ -17,27 +17,27 @@ public class Main {
         controleRadio.menosFrequecia();
         controleRadio.power();
 
-        // controleTV1.definirCanal(13);
-        // controleTV1.verCanal();
-        // controleTV1.verVolume();
-        // controleTV1.maisVolume();
-        // controleTV1.menosVolume();
-        // controleTV1.definirVolume(99);
-        // controleTV1.power();
-        // controleTV1.definirVolume(100);
-        // controleTV1.verCanal();
-        // controleTV1.definirCanal(2);
-        // controleTV1.definirCanal(100);
-        // controleTV1.maisCanais();
-        // controleTV1.maisCanais();
-        // controleTV1.menosCanais();
-        // controleTV1.verVolume();
-        // controleTV1.maisVolume();
-        // controleTV1.menosVolume();
-        // controleTV1.mute();
-        // controleTV1.maisVolume();
-        // controleTV1.menosVolume();
-        // controleTV1.power();
+        controleTV1.definirCanal(13);
+        controleTV1.verCanal();
+        controleTV1.verVolume();
+        controleTV1.maisVolume();
+        controleTV1.menosVolume();
+        controleTV1.definirVolume(99);
+        controleTV1.power();
+        controleTV1.definirVolume(100);
+        controleTV1.verCanal();
+        controleTV1.definirCanal(2);
+        controleTV1.definirCanal(100);
+        controleTV1.maisCanais();
+        controleTV1.maisCanais();
+        controleTV1.menosCanais();
+        controleTV1.verVolume();
+        controleTV1.maisVolume();
+        controleTV1.menosVolume();
+        controleTV1.mute();
+        controleTV1.maisVolume();
+        controleTV1.menosVolume();
+        controleTV1.power();
     
 
     }
