28a566e8daab5a5726890c5af4a0fbc29723c9d1
Mihay Victor <m.victorcezario@gmail.com>
2024-06-13 20:18:03 -0300

    update

--- src/interfacesContols/InterfaceControlTV.java
+++ src/interfacesContols/InterfaceControlTV.java
@@ -1,4 +1,4 @@
-package interfaces;
+package interfacesContols;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,6 +1,6 @@
 package main;
 
-import interfaces.InterfaceControlTV;
+import interfacesContols.InterfaceControlTV;
 
 public class Main {
      public static void main(String[] args) throws Exception {
